02b6e1eb8aab6e9c85bb18d2a2172114e02932ad
LTTHIEN\Administrator <le.trong.thien@quantic.com.vn>
2020-12-15 09:55:34 +0700

    push

--- src/verify-webhook.js
+++ src/verify-webhook.js
@@ -6,10 +6,16 @@ const verifyWebhook = (req, res) => {
   let challenge = req.query['hub.challenge'];
 
   if (mode && token === VERIFY_TOKEN) {
+    console.log(token)
+    console.log(mode)
+    console.log(mode)
     res.status(200).send(challenge);
   } else {
-      res.sendStatus(403);
-    }
+    console.log(token)
+    console.log(mode)
+    console.log(mode)
+    res.sendStatus(403);
+  }
 };
 
 module.exports = verifyWebhook;
